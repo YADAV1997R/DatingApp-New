b02848254de0505bfa8a9df505828c431614ff97
YADAV1997R <138108439+YADAV1997R@users.noreply.github.com>
2025-03-03 14:16:29 +0530

    End of Section -2

---
+++ API/Program.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// my connection String Services...
+builder.Services.AddDbContext<DataContext>(opt=>{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
